d2079abf46805da4d0a06b65db6f6b8226e61a25
Vince <vincentochieng33@gmail.com>
2024-04-10 18:15:05 +0300

    Initial Commit

--- app/src/main/java/com/example/newsnow/MainActivity.java
+++ app/src/main/java/com/example/newsnow/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.newsnow;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.kwabenaberko.newsapilib.NewsApiClient;
+import com.kwabenaberko.newsapilib.models.request.TopHeadlinesRequest;
+import com.kwabenaberko.newsapilib.models.response.ArticleResponse;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getNews();
+    }
+
+    void getNews(){
+        NewsApiClient newsApiClient = new NewsApiClient("22898a17604d448691cb122e1cb11afe");
+        newsApiClient.getTopHeadlines(
+                new TopHeadlinesRequest.Builder()
+                        .language("en")
+                        .build(),
+                new NewsApiClient.ArticlesResponseCallback() {
+                    @Override
+                    public void onSuccess(ArticleResponse response) {
+                        Log.i("Got RESPONSE",response.toString());
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        Log.i("Got Failure",throwable.getMessage());
+                    }
+                }
+        );
     }
 }
