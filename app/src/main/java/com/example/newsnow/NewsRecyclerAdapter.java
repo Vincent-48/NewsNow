318ce8b86eae4db2fbcca593bbed841324bec36a
Vince <vincentochieng33@gmail.com>
2024-04-15 19:34:16 +0300

    Initial Commit

--- app/src/main/java/com/example/newsnow/NewsRecyclerAdapter.java
+++ app/src/main/java/com/example/newsnow/NewsRecyclerAdapter.java
@@ -1,4 +1,42 @@
 package com.example.newsnow;
 
-public class NewsRecyclerAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NewsRecyclerAdapter extends RecyclerView.Adapter {
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class NewsViewHolder extends RecyclerView.ViewHolder{
+
+        TextView titleTextView, sourceTextView;
+        ImageView imageView;
+
+        public NewsViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            titleTextView = itemView.findViewById(R.id.article_title);
+            sourceTextView = itemView.findViewById(R.id.article_source);
+            imageView = itemView.findViewById(R.id.article_image_view);
+        }
+    }
 }
