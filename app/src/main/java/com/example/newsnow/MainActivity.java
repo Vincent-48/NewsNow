59beed0b779c660578b920fd52d0c307a96d5d31
Vince <vincentochieng33@gmail.com>
2024-04-15 20:25:41 +0300

    Initial Commit

--- app/src/main/java/com/example/newsnow/MainActivity.java
+++ app/src/main/java/com/example/newsnow/MainActivity.java
@@ -1,21 +1,42 @@
 package com.example.newsnow;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.material.progressindicator.LinearProgressIndicator;
 import com.kwabenaberko.newsapilib.NewsApiClient;
+import com.kwabenaberko.newsapilib.models.Article;
 import com.kwabenaberko.newsapilib.models.request.TopHeadlinesRequest;
 import com.kwabenaberko.newsapilib.models.response.ArticleResponse;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    List<Article>articleList;
+    NewsRecyclerAdapter adapter;
+    LinearProgressIndicator progressIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupRecyclerView();
         getNews();
+
+        recyclerView = findViewById(R.id.news_recycler_view);
+        progressIndicator = findViewById(R.id.progress_bar);
+    }
+
+    void setupRecyclerView(){
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new NewsRecyclerAdapter(articleList);
+        recyclerView.setAdapter(adapter);
     }
 
     void getNews(){
